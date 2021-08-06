e7793f8d58cb261e2bac1bdc5ded627ade84d791
possibleit <possibleit@outlook.com>
2021-08-06 22:39:56 +0800

    完成main layout

--- app/src/main/java/com/example/possibleit/bitbook/Homepage/MainActivity.java
+++ app/src/main/java/com/example/possibleit/bitbook/Homepage/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.possibleit.bitbook;
+package com.example.possibleit.bitbook.Homepage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.possibleit.bitbook.R;
 
 public class MainActivity extends AppCompatActivity {
 
